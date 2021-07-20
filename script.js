7fcaefcb18c87e5574d2a606c548c3b55e61ad73
cyberck <dushaev2017@gmail.com>
2021-07-20 00:17:23 -0700

    dom-2

---
+++ script.js
@@ -0,0 +1,133 @@
+// if(1=='1') {
+//     console.log('Privet');
+// }
+
+
+// var b  = prompt('vvedite lyuboe chislo')
+
+// if(b===5){
+//     console.log('znacheniye i tipi dannix ravni');
+// }else if (b==5){
+//     console.log('znacheniye ravni');
+// }else {
+//     console.log('nu bivaet');
+// }
+
+
+// var c = prompt ('vvedite sekretnoe slova');
+// if(c == 'admin') {
+//     console.log('vi ugadali');    
+// }else {
+//     console.log('sorri');
+// }
+
+
+// var c = prompt ('vvedite sekretnoe slova');
+
+// if(c == 'admin' || c == 'админ' || c == 'administrator') {
+//         console.log('vi ugadali');    
+//     }else {
+//         console.log('sorri');
+//     }
+
+
+// domashka 1
+
+
+
+// var age = +prompt('vvedite svoy vozrast');
+
+// if(age>0 && age <= 18) {
+//     alert('vi eshe molodi');
+// }
+// else if(age>18 && age<=50) {
+//     alert('vam nujno rabotat');
+// }
+// else if(age>50 && age<=59) {
+//     alert('vam skoro na pensiyu');
+// }
+// else if(age>59 && age<=150) {
+// alert('vi pensiyoner'); 
+// }
+// else {
+//     alert('Pozdravlyayu takogo vozrosta netu ')
+// }
+
+// domashka 2
+
+var num = +prompt('vvedite vremya i sifrah')
+
+switch (num) {
+    case 1:
+        alert('chas nochi');
+        break;
+    case 2:
+        alert('dva chasa nochi');
+        break;
+    case 3:
+        alert('tri chasa nochi');
+        break;
+    case 4:
+        alert('chetire chasa utra');
+        break;
+    case 5:
+        alert('pyat chasa utra');
+        break;
+    case 6:
+        alert('shest chasa utra');
+        break;
+    case 7:
+        alert('sem chasa utra');
+        break;
+    case 8:
+        alert('vosem chasa utra');
+        break;
+    case 9:
+        alert('devyat chasa utra');
+        break;
+    case 10:
+        alert('desyat chasa utra');
+        break;
+    case 11:
+        alert('odinatsat chasa obeda');
+        break;
+    case 12:
+        alert('dvinatsat chasa obeda');
+        break;
+    case 13:
+        alert('trinadsat chasa obeda')
+        break;
+    case 14:
+        alert('chetirnadsat chasa obeda')
+        break;
+    case 15:
+        alert('pyatnatsat chasa posle obeda')
+        break;
+    case 16:
+        alert('shaesnadsot chasa posle obeda')
+        break;
+    case 17:
+        alert('semnadsot chasa posle obeda')
+        break;
+    case 18: 
+        alert('vosemnadsot chasa posle obeda')
+        break;
+    case 19:
+        alert('devednadsot chasa vechera')
+        break;
+    case 20:
+        alert('dvatsat chasa vechera')
+        break;
+    case 21:
+        alert('dvatsat odin chasa vechera')
+        break;
+    case 22:
+        alert('dvatsat dva chasa nochi')
+        break;
+    case 23: 
+        alert('dvatsat tri chasa nochi')     
+        break;
+    case 24:
+        alert('dvatsat chetire chasa nohi')
+        break;    
+}
