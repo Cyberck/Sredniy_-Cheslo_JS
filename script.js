b8878553515a7899a0c0af61db99c0ece9452234
cyberck <dushaev2017@gmail.com>
2021-08-24 11:10:40 -0700

    trt

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+
+var a = +prompt('vvedite pervoe chislo')
+var b = +prompt('vvedite vtoroe chislo')
+var c = +prompt('vvedite trete cheslo')
+
+if(a > c && a < b || a < c && a > b) {
+    console.log('srednoe chislo ' +a);
+}else if(b > c && b < a || b < c && b > a) {
+    console.log('srednoe checlo ' +b);    
+}else if (c > b && c < a || c < a && c > b) {
+    console.log('srednoe checlo ' +c);
+}else {
+    console.log('error');
+}
+
+
+
+
 // if(1=='1') {
 //     console.log('Privet');
 // }
@@ -55,79 +74,119 @@
 
 // domashka 2
 
-var num = +prompt('vvedite vremya i sifrah')
+// var num = +prompt('vvedite vremya i sifrah')
 
-switch (num) {
-    case 1:
-        alert('chas nochi');
-        break;
-    case 2:
-        alert('dva chasa nochi');
-        break;
-    case 3:
-        alert('tri chasa nochi');
-        break;
-    case 4:
-        alert('chetire chasa utra');
-        break;
-    case 5:
-        alert('pyat chasa utra');
-        break;
-    case 6:
-        alert('shest chasa utra');
-        break;
-    case 7:
-        alert('sem chasa utra');
-        break;
-    case 8:
-        alert('vosem chasa utra');
-        break;
-    case 9:
-        alert('devyat chasa utra');
-        break;
-    case 10:
-        alert('desyat chasa utra');
-        break;
-    case 11:
-        alert('odinatsat chasa obeda');
-        break;
-    case 12:
-        alert('dvinatsat chasa obeda');
-        break;
-    case 13:
-        alert('trinadsat chasa obeda')
-        break;
-    case 14:
-        alert('chetirnadsat chasa obeda')
-        break;
-    case 15:
-        alert('pyatnatsat chasa posle obeda')
-        break;
-    case 16:
-        alert('shaesnadsot chasa posle obeda')
-        break;
-    case 17:
-        alert('semnadsot chasa posle obeda')
-        break;
-    case 18: 
-        alert('vosemnadsot chasa posle obeda')
-        break;
-    case 19:
-        alert('devednadsot chasa vechera')
-        break;
-    case 20:
-        alert('dvatsat chasa vechera')
-        break;
-    case 21:
-        alert('dvatsat odin chasa vechera')
-        break;
-    case 22:
-        alert('dvatsat dva chasa nochi')
-        break;
-    case 23: 
-        alert('dvatsat tri chasa nochi')     
-        break;
-    case 24:
-        alert('dvatsat chetire chasa nohi')
-        break;    
-}
+// switch (num) {
+//     case 1:
+//         alert('chas nochi');
+//         break;
+//     case 2:
+//         alert('dva chasa nochi');
+//         break;
+//     case 3:
+//         alert('tri chasa nochi');
+//         break;
+//     case 4:
+//         alert('chetire chasa utra');
+//         break;
+//     case 5:
+//         alert('pyat chasa utra');
+//         break;
+//     case 6:
+//         alert('shest chasa utra');
+//         break;
+//     case 7:
+//         alert('sem chasa utra');
+//         break;
+//     case 8:
+//         alert('vosem chasa utra');
+//         break;
+//     case 9:
+//         alert('devyat chasa utra');
+//         break;
+//     case 10:
+//         alert('desyat chasa utra');
+//         break;
+//     case 11:
+//         alert('odinatsat chasa obeda');
+//         break;
+//     case 12:
+//         alert('dvinatsat chasa obeda');
+//         break;
+//     case 13:
+//         alert('trinadsat chasa obeda')
+//         break;
+//     case 14:
+//         alert('chetirnadsat chasa obeda')
+//         break;
+//     case 15:
+//         alert('pyatnatsat chasa posle obeda')
+//         break;
+//     case 16:
+//         alert('shaesnadsot chasa posle obeda')
+//         break;
+//     case 17:
+//         alert('semnadsot chasa posle obeda')
+//         break;
+//     case 18: 
+//         alert('vosemnadsot chasa posle obeda')
+//         break;
+//     case 19:
+//         alert('devednadsot chasa vechera')
+//         break;
+//     case 20:
+//         alert('dvatsat chasa vechera')
+//         break;
+//     case 21:
+//         alert('dvatsat odin chasa vechera')
+//         break;
+//     case 22:
+//         alert('dvatsat dva chasa nochi')
+//         break;
+//     case 23: 
+//         alert('dvatsat tri chasa nochi')     
+//         break;
+//     case 24:
+//         alert('dvatsat chetire chasa nohi')
+//         break;
+//     default:
+//         alert('takogo vremeni netu')        
+// }
+
+// let i = 0;
+// function rek() {
+//     console.log(i);
+// if(i < 100) {
+//     i++
+//     rek();
+// }
+// }
+// rek();
+
+
+
+
+
+
+          // domashka
+
+// var a = +prompt('vvedite pervoe chislo')
+// var b = +prompt('vvedite vtoroe chislo')
+// var c = +prompt('vvedite trete cheslo')
+
+// if(a > c && a < b || a < c && a > b) {
+//     console.log('srednoe chislo' +a);
+// }else if(b > c && b < a || b < c && b > a) {
+//     console.log('srednoe checlo' +b);    
+// }else if (c > b && c < a || c > a && c < b) {
+//     console.log('srednoe checlo' +c);
+// }else {
+//     console.log('error');
+// }
+
+        // sikli
+
+// for(var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
